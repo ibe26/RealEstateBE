ac2034ee7338ff693f9967dd7e0def164690ae0c
ilker <ilkerberke1212@gmail.com>
2023-10-23 12:01:42 +0300

    Fixed minor comment error

--- RealEstateBE/Entities/Property.cs
+++ RealEstateBE/Entities/Property.cs
@@ -3,30 +3,30 @@ using System.ComponentModel.DataAnnotations;
 
 namespace RealEstateBE.Model
 {
-    public class Property
+    public class Property:PropertyDTO
     {
         [Required, Key]
         public int PropertyID { get; set; }
-        [Required]
-        public string PropertyName { get; set; } = String.Empty;
-        [Required]
-        public int PropertyTypeID { get; set; }
-        [Required]
-        public int PropertyListingTypeID { get; set; }
-        [Required]
-        public int PropertyPrice { get; set; }
-        [Required]
-        public Int16 BedroomCount { get; set; }
-        [Required]
-        public Int16 BathroomCount { get; set; }
-        [Required]
-        public int Size { get; set; }
-        [Required]
-        public string City { get; set; } = String.Empty;
-        [Required]
-        public string District { get; set; } = String.Empty;
-        [Required]
-        public string Quarter { get; set; } = String.Empty;
+        //[Required]
+        //public string PropertyName { get; set; } = String.Empty;
+        //[Required]
+        //public int PropertyTypeID { get; set; }
+        //[Required]
+        //public int PropertyListingTypeID { get; set; }
+        //[Required]
+        //public int PropertyPrice { get; set; }
+        //[Required]
+        //public Int16 BedroomCount { get; set; }
+        //[Required]
+        //public Int16 BathroomCount { get; set; }
+        //[Required]
+        //public int Size { get; set; }
+        //[Required]
+        //public string City { get; set; } = String.Empty;
+        //[Required]
+        //public string District { get; set; } = String.Empty;
+        //[Required]
+        //public string Quarter { get; set; } = String.Empty;
 
     }
 }
